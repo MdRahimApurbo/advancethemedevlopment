2b1484a1b691589cb12a3a175d509ad55d2871b3
Md Rahim Apurbo <168536521+apurbo-omnixima@users.noreply.github.com>
2024-08-24 00:27:14 +0600

    #10

--- functions.php
+++ functions.php
@@ -12,8 +12,8 @@ function aquila_enqueue_scripts()
     wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
     wp_register_style('main-css', get_template_directory_uri() . '/assets/src/library/css/main.css', [], filemtime(get_template_directory() . '/assets/src/library/css/main.css'), 'all');
     wp_register_style('bootstrap-min-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
-    wp_register_script('main-js', get_template_directory_uri() . '/assets/src/library/js/main.js', [], filemtime(get_template_directory() . '/assets/src/library/js/main.js'), 'all');
-    wp_register_script('bootstrap-min-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, 'all');
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/src/library/js/main.js', [], filemtime(get_template_directory() . '/assets/src/library/js/main.js'), true);
+    wp_register_script('bootstrap-min-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
 
     wp_enqueue_style('style-css');
     wp_enqueue_style('main-css');
